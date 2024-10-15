7dafcb7b2866210ada6f585206a81c9100d34fdb
majogomezg <mariajogogar1@gmail.com>
2024-10-15 13:26:30 -0500

    fix telefono model validation

--- personapi-dotnet/Models/Entities/Telefono.cs
+++ personapi-dotnet/Models/Entities/Telefono.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
-namespace personapi_dotnet.Models.Entities;
-
-public partial class Telefono
+namespace personapi_dotnet.Models.Entities
 {
-    public string Num { get; set; } = null!;
+    public partial class Telefono
+    {
+        [Required(ErrorMessage = "El número es obligatorio.")]
+        [StringLength(15, ErrorMessage = "El número no puede tener más de 15 caracteres.")]
+        public string Num { get; set; } = null!;
 
-    public string Oper { get; set; } = null!;
+        [Required(ErrorMessage = "El operador es obligatorio.")]
+        [StringLength(45, ErrorMessage = "El operador no puede tener más de 45 caracteres.")]
+        public string Oper { get; set; } = null!;
 
-    public int Duenio { get; set; }
+        [Required(ErrorMessage = "El dueño es obligatorio.")]
+        public int Duenio { get; set; }
 
-    public virtual Persona DuenioNavigation { get; set; } = null!;
+        [ValidateNever]
+        public virtual Persona DuenioNavigation { get; set; } = null!;
+    }
 }
