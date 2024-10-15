5216b0de24914304cf450506bf5695a71c8f8234
majogomezg <mariajogogar1@gmail.com>
2024-10-15 13:27:40 -0500

    fix estudio model validation

--- personapi-dotnet/Models/Entities/Estudio.cs
+++ personapi-dotnet/Models/Entities/Estudio.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
-namespace personapi_dotnet.Models.Entities;
-
-public partial class Estudio
+namespace personapi_dotnet.Models.Entities
 {
-    public int IdProf { get; set; }
+    public partial class Estudio
+    {
+        [Required(ErrorMessage = "El Id de la Profesión es obligatorio.")]
+        public int IdProf { get; set; }
 
-    public int CcPer { get; set; }
+        [Required(ErrorMessage = "El Cc de la Persona es obligatorio.")]
+        public int CcPer { get; set; }
 
-    public DateOnly? Fecha { get; set; }
+        public DateOnly? Fecha { get; set; }
 
-    public string? Univer { get; set; }
+        [StringLength(50, ErrorMessage = "El nombre de la universidad no puede tener más de 50 caracteres.")]
+        public string? Univer { get; set; }
 
-    public virtual Persona CcPerNavigation { get; set; } = null!;
+        [ValidateNever]
+        public virtual Persona CcPerNavigation { get; set; } = null!;
 
-    public virtual Profesion IdProfNavigation { get; set; } = null!;
+        [ValidateNever]
+        public virtual Profesion IdProfNavigation { get; set; } = null!;
+    }
 }
